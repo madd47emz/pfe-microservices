7b3b4f3e29aabd9dc3f8d6df40f57a3c0b8a7b50
Mohamed Emziane <m.emziane@esi-sba.dz>
2024-06-10 17:44:01 +0100

    changing from openjdk 21 to openjdk 17

--- ms-2/src/main/java/dz/esisba/ms2/Ms2Application.java
+++ ms-2/src/main/java/dz/esisba/ms2/Ms2Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Ms2Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Ms2Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Ms2Application.class, args);
+	}
 
 }
